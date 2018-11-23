792f6b81e8d10bf3df9e694a7170ee6877d910f2
jposton <dev@jacobposton.com>
2018-11-23 03:27:20 -0500

    Implement empty KaH entry point state

---
+++ src/com/groupgames/web/states/kah/KahStartState.java
@@ -0,0 +1,30 @@
+package com.groupgames.web.states.kah;
+
+import com.groupgames.web.game.GameAction;
+import com.groupgames.web.game.State;
+import com.groupgames.web.game.StateManager;
+import com.groupgames.web.game.view.View;
+
+import java.util.Map;
+
+public class KahStartState extends State {
+
+    public KahStartState(StateManager manager, Map<String, Object> context) {
+        super(manager, context);
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public View getView(String uid) {
+        return null;
+    }
+
+    @Override
+    public void doAction(String uid, GameAction action) {
+
+    }
+}
