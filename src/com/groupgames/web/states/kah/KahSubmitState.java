950c55afb5c398e6f87372f75fee4d893af7672b
Griffin <griffingarratt@gmail.com>
2018-11-25 18:15:40 -0500

    Merge branches 'development' and 'websocket' of https://github.com/jposton96a/GroupGames into states
    # Conflicts:
    #	src/com/groupgames/web/webapp/servlets/JoinServlet.java

--- src/com/groupgames/web/states/kah/KahSubmitState.java
+++ src/com/groupgames/web/states/kah/KahSubmitState.java
@@ -5,8 +5,7 @@ import com.groupgames.web.game.State;
 import com.groupgames.web.game.StateManager;
 import com.groupgames.web.game.view.TemplateView;
 import com.groupgames.web.game.view.View;
-import com.groupgames.web.states.kah.actions.BeginGameAction;
-
+import com.groupgames.web.states.kah.actions.SubmitAction;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -15,9 +14,8 @@ import java.util.Map;
 import static com.groupgames.web.states.lobby.PlayerJoinState.GAME_CODE_TAG;
 import static com.groupgames.web.states.lobby.PlayerJoinState.USERS_TAG;
 
-public class KahStartState extends State {
-
-    public KahStartState(StateManager manager, Map<String, Object> context) {
+public class KahSubmitState extends State {
+    public KahSubmitState(StateManager manager, Map<String, Object> context) {
         super(manager, context);
     }
 
@@ -30,14 +28,14 @@ public class KahStartState extends State {
     public View getView(String uid, String webRootPath) {
         View view = null;
 
-        // Handle start view
+        // Handle submit view
         if(uid == null) {
             HashMap<String, Object> templateData = new HashMap<>();
             templateData.put("gamecode", getContext().get(GAME_CODE_TAG));
             templateData.put("users", getContext().get(USERS_TAG));
 
             try {
-                view = new TemplateView(webRootPath,"startgame.ftl", templateData);
+                view = new TemplateView(webRootPath,"submit.ftl", templateData);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -50,10 +48,10 @@ public class KahStartState extends State {
         Map<String, Object> context = this.getContext();
 
         switch (action.getType()) {
-            case "begin":
-                // Handles begin game action
-                BeginGameAction beginGameAction = new BeginGameAction(action);
-                manager.setState(new KahSubmitState(manager, context));
+            case "submit":
+                // Handles submit action
+                SubmitAction submitAction = new SubmitAction(action);
+                manager.setState(new KahVoteState(manager, context));
                 break;
 
         }
---
+++ src/com/groupgames/web/states/kah/actions/QuitAction.java
@@ -0,0 +1,17 @@
+package com.groupgames.web.states.kah.actions;
+
+import com.groupgames.web.game.GameAction;
+
+public class QuitAction extends GameAction{
+    private static final String PLAYER_UID_TAG = "uid";
+
+    public QuitAction(GameAction baseAction) throws IllegalArgumentException {
+        super(baseAction);
+
+        requireField(PLAYER_UID_TAG, String.class);
+    }
+
+    public String getSelected(){
+        return (String) parsed.get(PLAYER_UID_TAG);
+    }
+}
