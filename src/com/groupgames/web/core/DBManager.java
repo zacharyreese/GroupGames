f953072f837e19c26c20e25295d2553d819c063b
jposton <dev@jacobposton.com>
2018-11-25 20:07:41 -0500

    Move database access to singleton

--- src/com/groupgames/web/core/DBManager.java
+++ src/com/groupgames/web/core/DBManager.java
@@ -1,14 +1,29 @@
 package com.groupgames.web.core;
 
+import com.mysql.cj.xdevapi.DbDoc;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBManager {
+    private static final String dbUrl  = "jdbc:mysql://localhost/";
+    private static final String dbName = "groupgames";
+    private static final String dbUser = "gg_server";
+    private static final String dbPass = "password";
+
+    private static DBManager dbManagerInstance;
 
     private Connection conn = null;
 
-    public DBManager(String dbUrl, String dbName, String username, String password) {
+    public static DBManager getInstance(){
+        if (dbManagerInstance == null){
+            dbManagerInstance = new DBManager(dbUrl, dbName, dbUser, dbPass);
+        }
+        return dbManagerInstance;
+    }
+
+    private DBManager(String dbUrl, String dbName, String username, String password) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
 
