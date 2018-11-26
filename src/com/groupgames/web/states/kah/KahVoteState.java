207cf057dba0887fdf3f6f6b36142b7fb4a5dc94
Griffin <griffingarratt@gmail.com>
2018-11-26 02:54:32 -0500

    Merge branches 'development' and 'websocket' of https://github.com/jposton96a/GroupGames into states6
    # Conflicts:
    #	src/com/groupgames/web/webapp/servlets/JoinServlet.java
    #	web/WEB-INF/templates/lobby.ftl

--- src/com/groupgames/web/states/kah/KahVoteState.java
+++ src/com/groupgames/web/states/kah/KahVoteState.java
@@ -8,23 +8,29 @@ import com.groupgames.web.game.view.JsonView;
 import com.groupgames.web.game.view.TemplateView;
 import com.groupgames.web.game.view.View;
 import com.groupgames.web.states.kah.actions.VoteAction;
+import com.sun.org.apache.xerces.internal.xs.StringList;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.*;
 
 import static com.groupgames.web.states.lobby.PlayerJoinState.GAME_CODE_TAG;
 import static com.groupgames.web.states.lobby.PlayerJoinState.USERS_TAG;
 
 public class KahVoteState extends State {
+    private static final String SUBMIT_CARDS_TAG = "SubmitCard";
+    List<String> myList = new ArrayList<String>();
+    HashMap<String, Player> usersMap;
+
+
     Integer countdownTimer = 10;
     private static Timer timer;
-    HashMap<String, String> cardVotes = new HashMap<>();
+    HashMap<Integer, Integer> cardVotes = new HashMap<>();
 
     public KahVoteState(StateManager manager, Map<String, Object> context) {
         super(manager, context);
+
+        usersMap = (HashMap<String, Player>)getContext().get(USERS_TAG);
+
         timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
             public void run() {
@@ -59,8 +65,8 @@ public class KahVoteState extends State {
         // Handle vote view
         if(uid == null) {
             HashMap<String, Object> templateData = new HashMap<>();
-            templateData.put("gamecode", getContext().get(GAME_CODE_TAG));
-            templateData.put("users", getContext().get(USERS_TAG));
+            templateData.put("cards", getContext().get(SUBMIT_CARDS_TAG));
+            templateData.put("timer", countdownTimer);
 
             try {
                 view = new TemplateView(webRootPath,"playerHand.ftl", templateData);
@@ -74,15 +80,40 @@ public class KahVoteState extends State {
     @Override
     public void doAction(String uid, GameAction action) {
         Map<String, Object> context = this.getContext();
-
         switch (action.getType()) {
             case "vote":
                 // Handles submit action
                 VoteAction voteAction = new VoteAction(action);
-                manager.setState(new KahWinnerState(manager, context));
+
+                if(!myList.contains(uid)) {
+
+                    int count = cardVotes.get(voteAction.getSelected());
+                    cardVotes.put(voteAction.getSelected(), count + 1);
+                    myList.add(uid);
+
+                }
+                if(myList.size()==usersMap.size()){
+
+
+                    manager.setState(new KahWinnerState(manager, context));
+                }
+
                 break;
 
         }
     }
+    private Integer getVotedCard() {
+        Map.Entry<Integer, Integer> firstEntry = cardVotes.entrySet().iterator().next();
+        int largestKey = firstEntry.getKey();
+        Integer largestKeyValue = firstEntry.getValue();
 
+        for (Map.Entry<Integer, Integer> entry : cardVotes.entrySet()) {
+            int value = entry.getValue();
+            if (value > largestKeyValue) {
+                largestKey = entry.getKey();
+                largestKeyValue = entry.getValue();            }
+        }
+        return largestKey;
+
+    }
 }
